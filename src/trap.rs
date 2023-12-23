e384663f45a8708952b8283b1e25a50390e7b0c1
CharlyCst <charly.castes@epfl.ch>
2023-12-23 10:32:10 +0100

    Turn MCause into an enum
    This makes it easier to pattern match on mcause. We do loose information
    about the trap number for unknown traps, so maybe we alse want a
    function to return the raw mcause.

--- src/trap.rs
+++ src/trap.rs
@@ -2,7 +2,10 @@
 
 use core::fmt;
 
-use crate::{arch::{Arch, Architecture}, platform::exit_success};
+use crate::{
+    arch::{Arch, Architecture},
+    platform::exit_success,
+};
 
 // —————————————————————————————— Trap Handler —————————————————————————————— //
 
@@ -15,14 +18,13 @@ pub(crate) extern "C" fn trap_handler() {
     log::info!("  mepc:    0x{:x}", Arch::read_mepc());
     log::info!("  mtval:   0x{:x}", Arch::read_mtval());
 
-    match Arch::read_mcause().0 {
-        11 => {
-            // ECall from M-mode
+    match Arch::read_mcause() {
+        MCause::EcallFromMMode => {
             // For now we just exit successfuly
             log::info!("Success!");
             exit_success();
         }
-        _ => () // Continue
+        _ => (), // Continue
     }
 
     // Skip instruction and return
@@ -36,15 +38,75 @@ pub(crate) extern "C" fn trap_handler() {
 // ————————————————————————————————— mcause ————————————————————————————————— //
 
 #[derive(Clone, Copy)]
-pub struct MCause(usize);
+#[repr(usize)]
+pub enum MCause {
+    // Exceptions
+    InstrAddrMisaligned = 0,
+    InstrAccessFault = 1,
+    IllegalInstr = 2,
+    Breakpoinnt = 3,
+    LoadAddrMisaligned = 4,
+    LoadAccessFault = 5,
+    StoreMisaligned = 6,
+    StoreAccessFault = 7,
+    EcallFromUMode = 8,
+    EcallFromSMode = 9,
+    EcallFromMMode = 11,
+    InstrPageFault = 12,
+    LoadPageFault = 13,
+    StorePageFault = 15,
+    UnknownException = 16,
+
+    // Interrupts
+    UserSoftInt,
+    SupervisorSoftInt,
+    MachineSoftInt,
+    UserTimerInt,
+    SupervisorTimerInt,
+    MachineTimerInt,
+    UserExternalInt,
+    SupervisorExternalInt,
+    MachineExternalInt,
+    UnknownInt,
+}
 
 impl MCause {
     pub fn new(cause: usize) -> Self {
-        Self(cause)
-    }
-
-    pub fn value(self) -> usize {
-        self.0
+        if (cause as isize) < 0 {
+            // Interrupt
+            // TODO: I think this does not work, investigate when getting unknownn ints
+            match cause {
+                0 => Self::UserSoftInt,
+                1 => Self::SupervisorSoftInt,
+                3 => Self::MachineSoftInt,
+                4 => Self::UserTimerInt,
+                5 => Self::SupervisorTimerInt,
+                7 => Self::MachineTimerInt,
+                8 => Self::UserExternalInt,
+                9 => Self::SupervisorExternalInt,
+                11 => Self::MachineExternalInt,
+                _ => Self::UnknownInt,
+            }
+        } else {
+            // Trap
+            match cause {
+                0 => Self::InstrAddrMisaligned,
+                1 => Self::InstrAccessFault,
+                2 => Self::IllegalInstr,
+                3 => Self::Breakpoinnt,
+                4 => Self::LoadAddrMisaligned,
+                5 => Self::LoadAccessFault,
+                6 => Self::StoreMisaligned,
+                7 => Self::StoreAccessFault,
+                8 => Self::EcallFromUMode,
+                9 => Self::EcallFromSMode,
+                11 => Self::EcallFromMMode,
+                12 => Self::InstrPageFault,
+                13 => Self::LoadPageFault,
+                15 => Self::StorePageFault,
+                _ => Self::UnknownException,
+            }
+        }
     }
 }
 
@@ -52,40 +114,34 @@ impl MCause {
 
 impl fmt::Debug for MCause {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let val = self.value();
-        if (val as isize) < 0 {
-            // Interrupt
-            match val {
-                0 => write!(f, "user software interrupt"),
-                1 => write!(f, "supervisor software interrupt"),
-                3 => write!(f, "machine software interrupt"),
-                4 => write!(f, "user timer interrupt"),
-                5 => write!(f, "supervisor timer interrupt"),
-                7 => write!(f, "machine timer interrupt"),
-                8 => write!(f, "user external interrupt"),
-                9 => write!(f, "supervisor external interrupt"),
-                11 => write!(f, "machine external interrupt"),
-                _ => write!(f, "unknown interrupt: 0x{:x}", val),
-            }
-        } else {
-            // Trap
-            match val {
-                0 => write!(f, "instruction address misaligned"),
-                1 => write!(f, "instruction access fault"),
-                2 => write!(f, "illegal instruction"),
-                3 => write!(f, "breakpoint"),
-                4 => write!(f, "load address misaligned"),
-                5 => write!(f, "load access fault"),
-                6 => write!(f, "store/amo misaligned"),
-                7 => write!(f, "store/amo access fault"),
-                8 => write!(f, "ecall from u-mode"),
-                9 => write!(f, "ecall from s-mode"),
-                11 => write!(f, "ecall from m-mode"),
-                12 => write!(f, "instruction page fault"),
-                13 => write!(f, "load page fault"),
-                15 => write!(f, "store/amo page fault"),
-                _ => write!(f, "unknown exception: 0x{:x}", val),
-            }
+        match self {
+            // Interrupts
+            MCause::UserSoftInt => write!(f, "user software interrupt"),
+            MCause::SupervisorSoftInt => write!(f, "supervisor software interrupt"),
+            MCause::MachineSoftInt => write!(f, "machine software interrupt"),
+            MCause::UserTimerInt => write!(f, "user timer interrupt"),
+            MCause::SupervisorTimerInt => write!(f, "supervisor timer interrupt"),
+            MCause::MachineTimerInt => write!(f, "machine timer interrupt"),
+            MCause::UserExternalInt => write!(f, "user external interrupt"),
+            MCause::SupervisorExternalInt => write!(f, "supervisor external interrupt"),
+            MCause::MachineExternalInt => write!(f, "machine external interrupt"),
+            MCause::UnknownInt => write!(f, "unknown interrupt"),
+            // Traps
+            MCause::InstrAddrMisaligned => write!(f, "instruction address misaligned"),
+            MCause::InstrAccessFault => write!(f, "instruction access fault"),
+            MCause::IllegalInstr => write!(f, "illegal instruction"),
+            MCause::Breakpoinnt => write!(f, "breakpoint"),
+            MCause::LoadAddrMisaligned => write!(f, "load address misaligned"),
+            MCause::LoadAccessFault => write!(f, "load access fault"),
+            MCause::StoreMisaligned => write!(f, "store/amo misaligned"),
+            MCause::StoreAccessFault => write!(f, "store/amo access fault"),
+            MCause::EcallFromUMode => write!(f, "ecall from u-mode"),
+            MCause::EcallFromSMode => write!(f, "ecall from s-mode"),
+            MCause::EcallFromMMode => write!(f, "ecall from m-mode"),
+            MCause::InstrPageFault => write!(f, "instruction page fault"),
+            MCause::LoadPageFault => write!(f, "load page fault"),
+            MCause::StorePageFault => write!(f, "store/amo page fault"),
+            MCause::UnknownException => write!(f, "unknown exception"),
         }
     }
 }
