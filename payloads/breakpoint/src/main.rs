670f2773cc0560069f3ec802bc1a9eb24c583eaf
Charly Castes <charly.castes@epfl.ch>
2024-03-09 22:11:11 +0100

    Simple breakpoint test payload
    This PR adds a simple payload that causes a breakpoint exception and tries to
    handle it. The test fail on the current main, this commit is just there to
    demonstrate how to write such a payload and is not intended to be merged.
    
    NOTE: because trap handling is not yet implemented, the payload is not yet
    added to `just test`, but can be run with `just run breakpoint`

---
+++ payloads/breakpoint/src/main.rs
@@ -0,0 +1,62 @@
+#![no_std]
+#![no_main]
+
+use core::arch::{asm, global_asm};
+use core::panic::PanicInfo;
+
+use mirage_abi::{failure, success};
+
+global_asm!(
+    r#"
+.text
+.align 4
+.global _start
+_start:
+    j {entry}
+"#,
+    entry = sym entry,
+);
+
+extern "C" fn entry() -> ! {
+    unsafe {
+        let handler = _raw_breakpoint_trap_handler as usize;
+        // Let's rise an exception breakpoint directly
+        asm!(
+            "csrw mtvec, {0}", // Write mtvec
+            "ebreak",          // Cause an exception
+            in(reg) handler,
+        );
+    }
+
+    panic!();
+}
+
+/// This function should be called from the raw trap handler
+extern "C" fn trap_handler() {
+    // TODO: check mPP, and others?
+    success();
+}
+
+// —————————————————————————————— Trap Handler —————————————————————————————— //
+
+global_asm!(
+    r#"
+.text
+.align 4
+.global _raw_breakpoint_trap_handler
+_raw_breakpoint_trap_handler:
+    j {trap_handler} // Return imediately
+"#,
+    trap_handler = sym trap_handler,
+);
+
+extern "C" {
+    fn _raw_breakpoint_trap_handler();
+}
+
+// ————————————————————————————— Panic Handler —————————————————————————————— //
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    failure();
+}
