14c6507211c5801114acd704b2a3dcf47585edaf
Charly Castes <charly.castes@epfl.ch>
2024-03-21 15:33:31 +0100

    Adding more test payloads (debug, mret, mcause, mepc, mconfigpts, mtselect)
    Now we got all the pieces needed to add a new bunch of tests, those are now
    enabled by default (including the breakpoint test!).
    
    Co-authored-by: Abel Vexina Wilkinson <abel.vexinawilkinson@epfl.ch>

---
+++ payloads/mcause/src/main.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+
+use core::arch::{asm, global_asm};
+use core::panic::PanicInfo;
+use core::usize;
+
+use mirage_abi::{failure, success};
+
+global_asm!(
+    r#"
+.text
+.align 4
+.global _start
+_start:
+    j {entry}
+"#,
+    entry = sym entry,
+);
+
+extern "C" fn entry() -> ! {
+    let secret1: usize = 0x42;
+    let mut res: usize;
+    unsafe {
+        asm!(
+            "li {0}, 0x42",
+            "csrw mcause, {0}",
+            "csrr {1}, mcause",
+            in(reg) secret1,
+            out(reg) res,
+        );
+    }
+
+    read_test(res, 0);
+
+    let secret2: usize = 0x9;
+
+    unsafe {
+        asm!(
+            "li {0}, 0x9",
+            "csrw mcause, {0}",
+            "csrr {1}, mcause",
+            in(reg) secret2,
+            out(reg) res,
+        );
+    }
+
+    read_test(res, secret2);
+
+    unsafe {
+        asm!(
+            "li {0}, 0x8000000000000009",
+            "csrw mcause, {0}",
+            "csrr {1}, mcause",
+            in(reg) secret2,
+            out(reg) res,
+        );
+    }
+
+    read_test(res, secret2);
+
+    success();
+}
+
+fn read_test(out_csr: usize, expected: usize) {
+    assert_eq!(out_csr, expected);
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    failure();
+}
---
+++ payloads/mepc/src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+
+use core::arch::{asm, global_asm};
+use core::panic::PanicInfo;
+use core::usize;
+
+use mirage_abi::{failure, success};
+
+global_asm!(
+    r#"
+.text
+.align 4
+.global _start
+_start:
+    j {entry}
+"#,
+    entry = sym entry,
+);
+
+extern "C" fn entry() -> ! {
+    let secret: usize = 0x42;
+    let res: usize;
+    unsafe {
+        asm!(
+            "li {0}, 0x42",
+            "csrw mepc, {0}",
+            "csrr {1}, mepc",
+            in(reg) secret,
+            out(reg) res,
+        );
+    }
+
+    if res == secret {
+        success();
+    }
+
+    panic!();
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    failure();
+}
