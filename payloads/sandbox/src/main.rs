c6fd3ca8c7b0790dbd3028340ff0792220a12121
Abel Vexina Wilkinson <abel.vexinawilkinson@epfl.ch>
2024-05-17 19:08:23 +0200

    Add firmware test for PMP protection of Mirage
    The test checks that the firmware is properly sandboxed and can't access
    Mirage's own test and data directly. For now we only test direct access,
    but in the future we should add more exhaustive checks, for instance
    using Mirage ABI should not allow logging Mirage's own code or data.
    
    
    Co-authored-by: Charly Castes <charly.castes@epfl.ch>

---
+++ payloads/sandbox/src/main.rs
@@ -0,0 +1,58 @@
+//! Sandboxing test
+//!
+//! This test firmware checks that Mirage is properly protected, or in other words that the
+//! firmware is properly sandboxed.
+
+#![no_std]
+#![no_main]
+
+use core::arch::{asm, global_asm};
+use core::ptr;
+
+use mirage_abi::{setup_payload, success};
+
+setup_payload!(main);
+
+fn main() -> ! {
+    let mut mcause: usize;
+    unsafe {
+        // Setup trap handler
+        asm!(
+            "csrw mtvec, {handler}",
+            handler = in(reg) _raw_trap_handler as usize,
+        );
+
+        // Try to read an address that should be protected by Mirage
+        let x: usize = 0x80002000;
+        ptr::read_volatile(x as *const usize);
+
+        // Check what error we encountered
+        asm!(
+            "csrr t6, mcause",
+            out("t6") mcause,
+        );
+    }
+
+    assert_eq!(mcause, 0x5, "exception was not access fault");
+
+    success();
+}
+
+// —————————————————————————————— Trap Handler —————————————————————————————— //
+
+global_asm!(
+    r#"
+.text
+.align 4
+.global _raw_trap_handler
+_raw_trap_handler:
+    csrr t6, mepc  // Read EPC
+    addi t6, t6, 4 // Increment return pointer
+    csrw mepc, t6  // Write it back
+    mret
+"#,
+);
+
+extern "C" {
+    fn _raw_trap_handler();
+}
