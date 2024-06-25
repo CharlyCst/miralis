f332c563f1e77f01819920c926bb1084ea97d46d
François Costa <frankost.costa@gmail.com>
2024-06-25 16:34:59 +0200

    Fine grained logger with variable levels per module
    This commits allows to log specific modules at a lower level. For example the global level could be info but you want to have a few modules with trace for debugging purposes. You can add those modules in the toml configuration and logs will be printed at a higher granularity

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 //! Configuration constants
 //!
-//! The constants in this files are parsed from the Mirage configuration file (passed through
+//! The constants in this file are parsed from the Mirage configuration file (passed through
 //! environment variables by the runner during Mirage build).
 
 // ———————————————————————————————— Helpers ————————————————————————————————— //
@@ -60,7 +60,7 @@ const POW10: [usize; 20] = {
     array
 };
 
-const fn parse(env_var: Option<&str>) -> Option<usize> {
+const fn parse_usize(env_var: Option<&str>) -> Option<usize> {
     let Some(env_var) = env_var else {
         return None;
     };
@@ -86,13 +86,78 @@ const fn parse(env_var: Option<&str>) -> Option<usize> {
     Some(result)
 }
 
+/// Split a string of comma (",") separated values into a list of strings slices.
+const fn parse_str_list<const LEN: usize>(env_var: Option<&str>) -> [&str; LEN] {
+    // First we unwrap the option
+    let env_var = match env_var {
+        Some(var) => var,
+        None => return [""; LEN],
+    };
+
+    // Then we iterate over the bytes of the string
+    let bytes = env_var.as_bytes();
+    let mut res: [&str; LEN] = [""; LEN];
+    let mut idx_start = 0;
+    let mut idx_curr = 0;
+    let mut i = 0;
+
+    while i < LEN {
+        // We continue until we find a "," delimiter
+        while idx_curr < bytes.len() && bytes[idx_curr] != b',' {
+            idx_curr += 1;
+        }
+
+        // Then we need to get a sub-slice
+        // Unfortunately indexing with a range (`my_str[a..b]`) is not possible
+        // in const contexts yet, but splitting at a given index is. So we split
+        // it twice to get a sub-slice.
+        let sub_slice = &bytes.split_at(idx_start).1;
+        let sub_slice = &sub_slice.split_at(idx_curr - idx_start).0;
+
+        // We convert it back into a string, and unwrap it (again, `.unwrap()`
+        // is not available).
+        let sub_str = match core::str::from_utf8(sub_slice) {
+            Ok(sub_str) => sub_str,
+            Err(_) => panic!("Invalid string list in configuration"),
+        };
+        res[i] = sub_str;
+
+        // And we move on to the next element
+        idx_curr += 1;
+        idx_start = idx_curr;
+        i += 1;
+    }
+
+    res
+}
+
+/// Returns the len of a list of comma (",") separated values.
+const fn str_list_len(env_var: Option<&str>) -> usize {
+    // First we unwrap the option
+    let env_var = match env_var {
+        Some(var) => var,
+        None => return 0,
+    };
+
+    let mut len = 1;
+    let mut i = 0;
+    let bytes = env_var.as_bytes();
+    while i < bytes.len() {
+        if bytes[i] == b',' {
+            len += 1;
+        }
+        i += 1;
+    }
+    len
+}
+
 // ———————————————————————— Configuration Parameters ———————————————————————— //
 
 /// Weather the vCPU exposes S-mode.
 pub const VCPU_S_MODE: bool = is_enabled!("MIRAGE_VCPU_S_MODE");
 
 /// Maximum number of PMP exposed by the vCPU, no limit if None.
-pub const VCPU_MAX_PMP: Option<usize> = parse(option_env!("MIRAGE_VCPU_MAX_PMP"));
+pub const VCPU_MAX_PMP: Option<usize> = parse_usize(option_env!("MIRAGE_VCPU_MAX_PMP"));
 
 /// The desired log level.
 pub const LOG_LEVEL: Option<&'static str> = option_env!("MIRAGE_LOG_LEVEL");
@@ -101,4 +166,25 @@ pub const LOG_LEVEL: Option<&'static str> = option_env!("MIRAGE_LOG_LEVEL");
 pub const LOG_COLOR: bool = is_enabled!("MIRAGE_LOG_COLOR");
 
 /// The maximum number of firmware exits before quitting.
-pub const MAX_FIRMWARE_EXIT: Option<usize> = parse(option_env!("MIRAGE_DEBUG_MAX_FIRMWARE_EXITS"));
+pub const MAX_FIRMWARE_EXIT: Option<usize> =
+    parse_usize(option_env!("MIRAGE_DEBUG_MAX_FIRMWARE_EXITS"));
+
+/// Log error
+pub const LOG_ERROR: &[&str; str_list_len(option_env!("MIRAGE_LOG_ERROR"))] =
+    &parse_str_list(option_env!("MIRAGE_LOG_ERROR"));
+
+/// Log warn
+pub const LOG_WARN: &[&str; str_list_len(option_env!("MIRAGE_LOG_WARN"))] =
+    &parse_str_list(option_env!("MIRAGE_LOG_WARN"));
+
+/// Log info
+pub const LOG_INFO: &[&str; str_list_len(option_env!("MIRAGE_LOG_INFO"))] =
+    &parse_str_list(option_env!("MIRAGE_LOG_INFO"));
+
+/// Log debug
+pub const LOG_DEBUG: &[&str; str_list_len(option_env!("MIRAGE_LOG_DEBUG"))] =
+    &parse_str_list(option_env!("MIRAGE_LOG_DEBUG"));
+
+/// Log trace
+pub const LOG_TRACE: &[&str; str_list_len(option_env!("MIRAGE_LOG_TRACE"))] =
+    &parse_str_list(option_env!("MIRAGE_LOG_TRACE"));
