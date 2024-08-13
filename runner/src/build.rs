14db9763e143e8a87f4a044e29740485c920f7d4
Sofia <70857676+Wesdcv@users.noreply.github.com>
2024-08-13 17:28:39 +0200

    Now it is possible to explicitly build firmware With a specific config, as opposed to running miralis in qemu to build certain firmware Refs #135

--- runner/src/build.rs
+++ runner/src/build.rs
@@ -1,20 +1,33 @@
 //! Build
 
-use crate::artifacts::{build_target, Target};
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use crate::artifacts::{build_target, download_artifact, locate_artifact, Artifact, Target};
 use crate::config::read_config;
 use crate::BuildArgs;
 
 pub fn build(args: &BuildArgs) {
     let cfg = read_config(&args.config);
-    let miralis = build_target(Target::Miralis, &cfg);
-
-    if let Some(config) = &args.config {
-        println!(
-            "Built Miralis with config '{}', binary available at:",
-            config.display()
-        );
+    if let Some(firmware) = &args.firmware {
+        let firmware = match locate_artifact(firmware) {
+            Some(Artifact::Source { name }) => build_target(Target::Firmware(name), &cfg),
+            Some(Artifact::Downloaded { name, url }) => download_artifact(&name, &url),
+            None => PathBuf::from_str(firmware).expect("Invalid firmware path"),
+        };
+        println!("Built firmware, binary available at:");
+        println!("{}", firmware.display());
     } else {
-        println!("Built Miralis, binary available at:");
+        let miralis = build_target(Target::Miralis, &cfg);
+
+        if let Some(config) = &args.config {
+            println!(
+                "Built Miralis with config '{}', binary available at:",
+                config.display()
+            );
+        } else {
+            println!("Built Miralis, binary available at:");
+        }
+        println!("{}", miralis.display());
     }
-    println!("{}", miralis.display());
 }
