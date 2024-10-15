ade15cd5d753c6073a1af6d3ba1339e550de1015
Frederic Khayat <khayatfrederic@gmail.com>
2024-10-15 20:41:09 +0200

    Fix bug related to Sstc extension

--- src/virt.rs
+++ src/virt.rs
@@ -740,13 +740,6 @@ impl VirtContext {
             mstatus::MPP_FILTER,
             self.mode.to_bits(),
         );
-        Arch::write_csr(Csr::Mstatus, mstatus & !mstatus::MIE_FILTER);
-        Arch::write_csr(Csr::Mideleg, self.csr.mideleg);
-        Arch::write_csr(Csr::Medeleg, self.csr.medeleg);
-        Arch::write_csr(Csr::Mcounteren, self.csr.mcounteren);
-
-        Arch::write_csr(Csr::Mie, self.csr.mie);
-        Arch::write_csr(Csr::Mip, self.csr.mip);
 
         if mctx.hw.available_reg.senvcfg {
             Arch::write_csr(Csr::Senvcfg, self.csr.senvcfg);
@@ -755,6 +748,14 @@ impl VirtContext {
         if mctx.hw.available_reg.menvcfg {
             Arch::write_csr(Csr::Menvcfg, self.csr.menvcfg);
         }
+
+        Arch::write_csr(Csr::Mstatus, mstatus & !mstatus::MIE_FILTER);
+        Arch::write_csr(Csr::Mideleg, self.csr.mideleg);
+        Arch::write_csr(Csr::Medeleg, self.csr.medeleg);
+        Arch::write_csr(Csr::Mcounteren, self.csr.mcounteren);
+
+        Arch::write_csr(Csr::Mie, self.csr.mie);
+        Arch::write_csr(Csr::Mip, self.csr.mip);
 
         // If S extension is present - save the registers
         if mctx.hw.extensions.has_s_extension {
