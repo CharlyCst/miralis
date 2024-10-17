de6bbaed54fd6a8da21c092a879b879175e9c9b2
Frederic Khayat <khayatfrederic@gmail.com>
2024-10-17 09:47:44 +0200

    Start implementing Keystone policy

--- src/policy/keystone.rs
+++ src/policy/keystone.rs
@@ -2,25 +2,179 @@
 //!
 //! This policy module enforces the Keystone policies, i.e. it enables the creation of user-level
 //! enclaves by leveraging PMP for memory isolation.
+//! TODO: Remove allow(unused)
+
+use core::ptr;
 
 use crate::arch::Register;
 use crate::host::MiralisContext;
 use crate::policy::{PolicyHookResult, PolicyModule};
+use crate::virt::RegisterContextSetter;
 use crate::{RegisterContextGetter, VirtContext};
 
-/// The Keystone FID
+/// Keystone parameters
 ///
-/// See https://github.com/keystone-enclave/keystone/blob/80ffb2f9d4e774965589ee7c67609b0af051dc8b/sdk/include/shared/sm_call.h#L5C47-L5C57
-const KEYSTONE_FID: usize = 0x08424b45;
+/// See https://github.com/keystone-enclave/keystone/blob/80ffb2f9d4e774965589ee7c67609b0af051dc8b/sm/src/platform/generic/platform.h#L11
+const ENCL_MAX: usize = 16; // Maximum number of enclaves
+#[allow(unused)]
+const ENCLAVE_REGION_MAX: usize = 8;
+
+/// Keystone EID & FIDs
+///
+/// See https://github.com/keystone-enclave/keystone/blob/80ffb2f9d4e774965589ee7c67609b0af051dc8b/sdk/include/shared/sm_call.h#L5C1-L6C1
+mod sbi {
+    #![allow(unused)]
+    pub const KEYSTONE_EID: usize = 0x08424b45;
+
+    // 1999-2999 are called by host
+    pub const CREATE_ENCLAVE_FID: usize = 2001;
+    pub const DESTROY_ENCLAVE_FID: usize = 2002;
+    pub const RUN_ENCLAVE_FID: usize = 2003;
+    pub const RESUME_ENCLAVE_FID: usize = 2005;
+
+    // 2999-3999 are called by enclave
+    pub const RANDOM_FID: usize = 3001;
+    pub const ATTEST_ENCLAVE_FID: usize = 3002;
+    pub const GET_SEALING_KEY_FID: usize = 3003;
+    pub const STOP_ENCLAVE_FID: usize = 3004;
+    pub const EXIT_ENCLAVE_FID: usize = 3006;
+}
+
+/// Keystone return codes
+///
+/// See https://github.com/keystone-enclave/keystone/blob/master/sdk/include/shared/sm_err.h
+enum ReturnCode {
+    Success = 0,
+    NoFreeResources = 100013,
+    NotImplemented = 100100,
+}
+
+/// Enclave definitions
+///
+/// See https://github.com/keystone-enclave/keystone/blob/80ffb2f9d4e774965589ee7c67609b0af051dc8b/sm/src/enclave.h
+#[derive(Default)]
+#[allow(unused)]
+enum EnclaveState {
+    #[default]
+    Invalid = -1,
+    Destroying = 0,
+    Allocated,
+    Fresh,
+    Stopped,
+    Running,
+}
+
+#[derive(Default)]
+#[allow(unused)]
+enum EnclaveRegionType {
+    #[default]
+    Invalid,
+    Epm,
+    Utm,
+    Other,
+}
+
+#[derive(Default)]
+#[allow(unused)]
+struct RuntimeParams {
+    dram_base: usize,
+    dram_size: usize,
+    runtime_base: usize,
+    user_base: usize,
+    free_base: usize,
+    untrusted_base: usize,
+    untrusted_size: usize,
+    free_requested: usize,
+}
+
+#[derive(Default)]
+struct Enclave {
+    eid: usize,          // Enclave ID
+    state: EnclaveState, // Global state of the enclave
+    params: RuntimeParams,
+}
 
 /// The keystone policy module
 ///
 /// See https://keystone-enclave.org/
-pub struct KeystonePolicy {}
+#[derive(Default)]
+pub struct KeystonePolicy {
+    enclaves: [Enclave; ENCL_MAX],
+}
 
+impl KeystonePolicy {
+    /// Allocate an enclave slot and returns the index of the newly allocated enclave
+    fn allocate_enclave(&mut self) -> Result<usize, ReturnCode> {
+        for i in 0..ENCL_MAX {
+            let enclave = &mut self.enclaves[i];
+            if let EnclaveState::Invalid = enclave.state {
+                return Ok(i);
+            }
+        }
+
+        Err(ReturnCode::NoFreeResources)
+    }
+
+    fn create_enclave(&mut self, ctx: &mut VirtContext) -> ReturnCode {
+        log::debug!("Keystone: Create enclave");
+
+        // Read the arguments passed to create_enclave
+        #[repr(C)]
+        struct KeystoneRegion {
+            paddr: usize,
+            size: usize,
+        }
+
+        #[repr(C)]
+        struct CreateArgs {
+            epm_region: KeystoneRegion, // Enclave region
+            upm_region: KeystoneRegion, // Untrusted region
+
+            runtime_paddr: usize,
+            user_paddr: usize,
+            free_paddr: usize,
+            free_requested: usize,
+        }
+
+        // TODO: We should validate that the memory pointed by a0 is valid, and well aligned
+        // TODO: Check if args is valid (see enclave.c line 351)
+        let args = unsafe { ptr::read(ctx.get(Register::X10) as *const CreateArgs) };
+
+        // Create params
+        let params = RuntimeParams {
+            dram_base: args.epm_region.paddr,
+            dram_size: args.epm_region.size,
+            runtime_base: args.runtime_paddr,
+            user_base: args.user_paddr,
+            free_base: args.free_paddr,
+            untrusted_base: args.upm_region.paddr,
+            untrusted_size: args.upm_region.size,
+            free_requested: args.free_requested,
+        };
+
+        // Find a free enclave slot
+        let eid = match Self::allocate_enclave(self) {
+            Ok(index) => index,
+            Err(code) => return code,
+        };
+
+        self.enclaves[eid].eid = eid;
+        self.enclaves[eid].state = EnclaveState::Allocated;
+        self.enclaves[eid].params = params;
+
+        ReturnCode::Success
+    }
+
+    fn destroy_enclave(&mut self, _ctx: &mut VirtContext) -> ReturnCode {
+        log::debug!("Keystone: Destroy enclave");
+        ReturnCode::NotImplemented
+    }
+}
+
+/// To check how ecalls are handled, see https://github.com/riscv-software-src/opensbi/blob/2ffa0a153d804910c20b82974bfe2dedcf35a777/lib/sbi/sbi_ecall.c#L98
 impl PolicyModule for KeystonePolicy {
     fn init() -> Self {
-        KeystonePolicy {}
+        Self::default()
     }
 
     fn name() -> &'static str {
@@ -40,17 +194,42 @@ impl PolicyModule for KeystonePolicy {
         _mctx: &mut MiralisContext,
         ctx: &mut VirtContext,
     ) -> PolicyHookResult {
-        let fid = ctx.get(Register::X17);
-        if fid == KEYSTONE_FID {
-            ctx.pc += 4;
-            PolicyHookResult::Overwrite
-        } else {
-            PolicyHookResult::Ignore
+        let eid = ctx.get(Register::X17);
+        let fid = ctx.get(Register::X16);
+        if eid != sbi::KEYSTONE_EID {
+            return PolicyHookResult::Ignore;
         }
+
+        let return_code: ReturnCode = match fid {
+            sbi::CREATE_ENCLAVE_FID => Self::create_enclave(self, ctx),
+            sbi::DESTROY_ENCLAVE_FID => Self::destroy_enclave(self, ctx),
+            _ => {
+                log::debug!("Keystone: Unknown FID {}", fid);
+                ReturnCode::NotImplemented
+            }
+        };
+
+        ctx.set(Register::X10, return_code as usize);
+        ctx.pc += 4;
+
+        PolicyHookResult::Overwrite
     }
 
-    fn switch_from_payload_to_firmware(&mut self, _: &mut VirtContext, _: &mut MiralisContext) {}
+    fn switch_from_payload_to_firmware(
+        &mut self,
+        _ctx: &mut VirtContext,
+        _mctx: &mut MiralisContext,
+    ) {
+        // TODO: Implement
+    }
 
-    fn switch_from_firmware_to_payload(&mut self, _: &mut VirtContext, _: &mut MiralisContext) {}
+    fn switch_from_firmware_to_payload(
+        &mut self,
+        _ctx: &mut VirtContext,
+        _mctx: &mut MiralisContext,
+    ) {
+        // TODO: Implement
+    }
+
     const NUMBER_PMPS: usize = 0;
 }
