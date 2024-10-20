b781b9a89e22a7c6776f8a54f1a210873a32e942
Charly Castes <charly.castes@epfl.ch>
2024-10-20 21:05:47 +0200

    Replace manual const parsing with `from_str_radix`
    With Rust 1.82 the `from_str_radix` is now stable in const contexts. We
    can now use it to replace our old hand-rolled const integer parser
    without relying on unstable features.

--- crates/config_helpers/src/lib.rs
+++ crates/config_helpers/src/lib.rs
@@ -20,68 +20,15 @@ macro_rules! is_enabled {
 }
 
 // ————————————————————————————— String Parsing ————————————————————————————— //
-// Required to parse environment variables at compile time.
-// Can be removed once usize::from_str_radix stabilized as const, hopefully soon.
-// See https://github.com/rust-lang/rust/pull/124941
-//
-// Source (and license), adapted for Miralis:
-// https://gist.github.com/DutchGhost/d8604a3c796479777fe9f5e25d855cfd
-// —————————————————————————————————————————————————————————————————————————— //
-
-const fn parse_byte(b: u8, pow10: usize) -> usize {
-    let r = b - 48; // Remove ascii offset
-
-    if r > 9 {
-        panic!("Failed to parse config: expected usize")
-    } else {
-        (r as usize) * pow10
-    }
-}
-
-const POW10: [usize; 20] = {
-    let mut array = [0; 20];
-    let mut current = 1;
-
-    let mut index = 20;
-
-    loop {
-        index -= 1;
-        array[index] = current;
-
-        if index == 0 {
-            break;
-        }
-
-        current *= 10;
-    }
-
-    array
-};
 
 pub const fn parse_usize(env_var: Option<&str>) -> Option<usize> {
-    let Some(env_var) = env_var else {
-        return None;
-    };
-
-    let bytes = env_var.as_bytes();
-    let mut result: usize = 0;
-
-    let len = bytes.len();
-
-    // Start at the correct index of the table,
-    // (skip the power's that are too large)
-    let mut index_const_table = POW10.len().wrapping_sub(len);
-    let mut index = 0;
-
-    while index < env_var.len() {
-        let pow = POW10[index_const_table];
-        result += parse_byte(bytes[index], pow);
-
-        index += 1;
-        index_const_table += 1;
+    match env_var {
+        Some(value) => match usize::from_str_radix(value, 10) {
+            Ok(value) => Some(value),
+            Err(_) => panic!("Failed to parse integed from configuration"),
+        },
+        None => None,
     }
-
-    Some(result)
 }
 
 /// Split a string of comma (",") separated values into a list of strings slices.
