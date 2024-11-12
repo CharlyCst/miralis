4b1824fd4ed4e1035a9f864c67d78097a5f0703a
Charly Castes <charly.castes@epfl.ch>
2024-11-12 15:18:15 +0100

    Use distinct payload and firmware target
    In the past we merged the firmware and payload target files as they are
    now identical. However that caused more re-compilation than necessary
    because the firmware and the payload use different flags (in particular
    linker flags for the start address).
    
    This commit revert that changes and separate the config files for the
    payload and firmware. After this change the test time is reduced from
    30s to 10s on my machine.

--- runner/src/path.rs
+++ runner/src/path.rs
@@ -3,7 +3,7 @@
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
-use crate::artifacts::{Target, MIRALIS_TARGET, UNPRIVILEGED_TARGET};
+use crate::artifacts::{Target, FIRMWARE_TARGET, MIRALIS_TARGET, PAYLOAD_TARGET};
 use crate::config::Profiles;
 
 /// Return the root of the workspace.
@@ -28,7 +28,8 @@ pub fn get_target_dir_path(target: &Target, mode: Profiles) -> PathBuf {
     path.push("target");
     match target {
         Target::Miralis => path.push(MIRALIS_TARGET),
-        Target::Payload(_) | Target::Firmware(_) => path.push(UNPRIVILEGED_TARGET),
+        Target::Firmware(_) => path.push(FIRMWARE_TARGET),
+        Target::Payload(_) => path.push(PAYLOAD_TARGET),
     }
     match mode {
         Profiles::Debug => {
@@ -68,9 +69,8 @@ pub fn get_target_config_path(target: &Target) -> PathBuf {
     let mut path = get_misc_path();
     match target {
         Target::Miralis => path.push(format!("{}.json", MIRALIS_TARGET)),
-        Target::Firmware(_) | Target::Payload(_) => {
-            path.push(format!("{}.json", UNPRIVILEGED_TARGET))
-        }
+        Target::Firmware(_) => path.push(format!("{}.json", FIRMWARE_TARGET)),
+        Target::Payload(_) => path.push(format!("{}.json", PAYLOAD_TARGET)),
     }
     path
 }
