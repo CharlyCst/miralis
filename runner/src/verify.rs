e1f54cd69731daa88ce75d5e4cd02c5eaca4a80b
Charly Castes <charly.castes@epfl.ch>
2024-11-27 16:52:28 +0100

    Runner: add support for Kani
    This commit adds support for model checking with Kani. This is done from
    the runner so that we can simply ignore the model checking if Kani is
    not installed and we are not running in strict mode (e.g. in the CI).
    
    The Sail model is **not** included in this commit, so nothing can
    actually be checked with this commit.

---
+++ runner/src/verify.rs
@@ -0,0 +1,55 @@
+//! Verification through model checking
+
+use std::env;
+use std::process::{Command, ExitCode};
+
+use crate::{VerifyArgs, RUNNER_STRICT_MODE};
+
+pub fn verify(args: &mut VerifyArgs) -> ExitCode {
+    if env::var(RUNNER_STRICT_MODE).is_ok() && !args.strict {
+        log::info!("Runing checks in strict mode");
+        args.strict = true;
+    }
+
+    // When Kani is not installed we return an error in strict mode but simply mark it as a warning
+    // if not. This makes the tests pass in local for people who did not install Kani yet.
+    if !kani_is_available() {
+        let message = "Kani is not installed, could not run model checking";
+        if args.strict {
+            log::error!("{}", message);
+            return ExitCode::FAILURE;
+        } else {
+            log::warn!("{}", message);
+            return ExitCode::SUCCESS;
+        }
+    }
+
+    let mut kani_cmd = Command::new("cargo");
+    kani_cmd
+        .arg("kani")
+        .args(["--output-format", "terse"])
+        .args(["-p", "model_checking"]);
+
+    let exit_status = kani_cmd.status().expect("Failed to run Kani");
+    if exit_status.success() {
+        log::info!("Successfully passed verification");
+        ExitCode::SUCCESS
+    } else {
+        log::error!("Failed verification");
+        ExitCode::FAILURE
+    }
+}
+
+/// Returns true if Kani is available.
+pub fn kani_is_available() -> bool {
+    let mut kani_cmd = Command::new("cargo");
+    kani_cmd.arg("kani").arg("--version");
+    kani_cmd
+        .stdout(std::process::Stdio::null())
+        .stderr(std::process::Stdio::null());
+
+    kani_cmd
+        .status()
+        .ok()
+        .map_or(false, |status| status.success())
+}
