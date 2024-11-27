8d2f13d6b2d6ab16b88f764cf24c046ea8abc8ed
François Costa <frankost.costa@gmail.com>
2024-11-27 23:03:10 +0100

    wip another bug found

--- formal-verification/src/lib.rs
+++ formal-verification/src/lib.rs
@@ -14,8 +14,6 @@ impl SailVirtCtx {
     pub fn from(ctx: &mut VirtContext) -> Self {
         let mut sail_ctx = SailVirtCtx::new();
 
-
-
         sail_ctx.nextPC = BitVector::new(ctx.pc as u64);
         sail_ctx.PC = BitVector::new(ctx.pc as u64);
 
@@ -31,10 +29,10 @@ impl SailVirtCtx {
         sail_ctx.mip = BitField::new(ctx.csr.mip as u64);
         sail_ctx.mtvec = BitField::new(ctx.csr.mtvec as u64);
         sail_ctx.mscratch = BitVector::new(ctx.csr.mscratch as u64);
-        sail_ctx.mvendorid = BitVector::new(ctx.csr.mscratch as u64);
-        // sail_ctx.marchid = BitVector::new(ctx.csr.marchid as u64);
+        sail_ctx.mvendorid = BitVector::new(ctx.csr.mvendorid as u64);
+        sail_ctx.marchid = BitVector::new(ctx.csr.marchid as u64);
         sail_ctx.mimpid = BitVector::new(ctx.csr.mimpid as u64);
-        sail_ctx.mcycle = BitVector::new(ctx.csr.mcycle as u64);
+        /*sail_ctx.mcycle = BitVector::new(ctx.csr.mcycle as u64);
         sail_ctx.minstret = BitVector::new(ctx.csr.minstret as u64);
         sail_ctx.mcountinhibit = BitField::new(ctx.csr.mcountinhibit as u64);
         sail_ctx.mcounteren = BitField::new(ctx.csr.mcounteren as u64);
@@ -65,7 +63,7 @@ impl SailVirtCtx {
         // ctx.csr.pmpcfg = [kani::any(); 8]; 
         // ctx.csr.pmpaddr = [kani::any(); 64];
         // ctx.csr.mhpmcounter=  [kani::any(); 29];
-        // ctx.csr.mhpmevent=  [kani::any(); 29];
+        // ctx.csr.mhpmevent=  [kani::any(); 29];*/
 
         sail_ctx
     }
@@ -96,9 +94,9 @@ impl SailVirtCtx {
         ctx.csr.mtvec=self.mtvec.bits.bits() as usize;
         ctx.csr.mscratch=self.mscratch.bits() as usize;
         ctx.csr.mvendorid= self.mvendorid.bits() as usize;
-        // ctx.csr.marchid= self.marchid.bits() as usize;
+        ctx.csr.marchid= self.marchid.bits() as usize;
         ctx.csr.mimpid= self.mimpid.bits() as usize;
-        ctx.csr.mcycle= self.mcycle.bits() as usize;
+        /*ctx.csr.mcycle= self.mcycle.bits() as usize;
         ctx.csr.minstret= self.minstret.bits() as usize;
         ctx.csr.mcountinhibit= self.mcountinhibit.bits.bits() as usize;
         ctx.csr.mcounteren= self.mcounteren.bits.bits() as usize;
@@ -106,10 +104,7 @@ impl SailVirtCtx {
         // ctx.csr.mseccfg= self.mseccfg.bits.bits() as usize;
         ctx.csr.mcause= self.mcause.bits.bits() as usize;
         ctx.csr.mepc= self.mepc.bits() as usize;
-        ctx.csr.mtval= self.mtval.bits() as usize;
-        // ctx.csr.mtval2= self.mtval2.bits.bits() as usize;
-        ctx.csr.mstatus= self.mstatus.bits.bits() as usize;
-        // ctx.csr.mtinst= self.mtinst.bits.bits() as usize;
+        ctx.csr.mtval= self.mtval.bits() as usize;ext_write_CSR
         ctx.csr.mconfigptr= self.mconfigptr.bits() as usize;
         ctx.csr.stvec= self.stvec.bits.bits() as usize;
         ctx.csr.scounteren= self.scounteren.bits.bits() as usize;
@@ -125,7 +120,7 @@ impl SailVirtCtx {
         // ctx.csr.pmpcfg = [kani::any(); 8]; 
         // ctx.csr.pmpaddr = [kani::any(); 64];
         // ctx.csr.mhpmcounter=  [kani::any(); 29];
-        // ctx.csr.mhpmevent=  [kani::any(); 29];
+        // ctx.csr.mhpmevent=  [kani::any(); 29];*/
 
         ctx
     }
@@ -146,9 +141,11 @@ fn KaniVirtCtx() -> VirtContext {
     ctx.csr.mie= kani::any();
     ctx.csr.mip= kani::any();
     ctx.csr.mtvec= kani::any();
-    ctx.csr.marchid= kani::any();
-    ctx.csr.mimpid= kani::any();
-    ctx.csr.mcycle= kani::any();
+    ctx.csr.mscratch=kani::any();
+    ctx.csr.mvendorid= kani::any();
+    ctx.csr.marchid = 0;
+    // ctx.csr.mimpid= kani::any();
+    /*ctx.csr.mcycle= kani::any();
     ctx.csr.minstret= kani::any();
     ctx.csr.mcountinhibit= kani::any();
     ctx.csr.mcounteren= kani::any();
@@ -172,35 +169,10 @@ fn KaniVirtCtx() -> VirtContext {
     ctx.csr.scontext= kani::any();
     ctx.csr.medeleg= kani::any();
     ctx.csr.mideleg = mie::MIDELEG_READ_ONLY_ONE;
-    ctx.csr.hstatus= kani::any();
-    ctx.csr.hedeleg= kani::any();
-    ctx.csr.hideleg= kani::any();
-    ctx.csr.hvip= kani::any();
-    ctx.csr.hip= kani::any();
-    ctx.csr.hie= kani::any();
-    ctx.csr.hgeip= kani::any();
-    ctx.csr.hgeie= kani::any();
-    ctx.csr.henvcfg= kani::any();
-    ctx.csr.henvcfgh= kani::any();
-    ctx.csr.hcounteren= kani::any();
-    ctx.csr.htimedelta= kani::any();
-    ctx.csr.htimedeltah= kani::any();
-    ctx.csr.htval= kani::any();
-    ctx.csr.htinst= kani::any();
-    ctx.csr.hgatp= kani::any();
-    ctx.csr.vsstatus= kani::any();
-    ctx.csr.vsie= kani::any();
-    ctx.csr.vstvec= kani::any();
-    ctx.csr.vsscratch= kani::any();
-    ctx.csr.vsepc= kani::any();
-    ctx.csr.vscause= kani::any();
-    ctx.csr.vstval= kani::any();
-    ctx.csr.vsip= kani::any();
-    ctx.csr.vsatp= kani::any();
     ctx.csr.pmpcfg = [kani::any(); 8]; 
     ctx.csr.pmpaddr = [kani::any(); 64];
     ctx.csr.mhpmcounter=  [kani::any(); 29];
-    ctx.csr.mhpmevent=  [kani::any(); 29];
+    ctx.csr.mhpmevent=  [kani::any(); 29];*/
 
     ctx
 }
@@ -218,7 +190,7 @@ mod verification {
 
     // #[kani::proof]
     pub fn mret() {
-        /*let mpp = match kani::any::<u8>() % 3 {
+        let mpp = match kani::any::<u8>() % 3 {
             0 => 0b00,
             1 => 0b01,
             2 => 0b11,
@@ -250,13 +222,20 @@ mod verification {
 
         ctx.emulate_mret(&mut mctx);
 
-        assert_eq!(ctx, sail_ctx.into_virt_context(), "mret equivalence");*/
+        assert_eq!(ctx, sail_ctx.into_virt_context(), "mret equivalence");
+    }
+
+    fn filter_possible_instructions(value: usize) {
+
+
+
+
     }
 
     #[kani::proof]
     pub fn read_csr() {
         
-        let csr_register = 0b111100010010; // kani::any::<u64>() & ((1<<13) - 1);
+        let csr_register = 0b111100010011; // kani::any::<u64>() & ((1<<13) - 1);
 
         let mut ctx = KaniVirtCtx();
 
@@ -270,6 +249,39 @@ mod verification {
         let decoded_csr  = mctx.decode_csr(csr_register as usize);
         ctx.csr.marchid = ctx.get(decoded_csr);
 
-        assert_eq!(ctx.csr, sail_ctx.into_virt_context().csr, "read csr equivalence");
+        assert_eq!(ctx.csr.misa, sail_ctx.into_virt_context().csr.misa, "read csr equivalence");
+        assert_eq!(ctx.csr.mie, sail_ctx.into_virt_context().csr.mie, "read csr equivalence");
+        assert_eq!(ctx.csr.mip, sail_ctx.into_virt_context().csr.mip, "read csr equivalence");
+        assert_eq!(ctx.csr.mtvec, sail_ctx.into_virt_context().csr.mtvec, "read csr equivalence");
+        assert_eq!(ctx.csr.marchid, sail_ctx.into_virt_context().csr.marchid, "read csr equivalence");
+        assert_eq!(ctx.csr.mimpid, sail_ctx.into_virt_context().csr.mimpid, "read csr equivalence");
+        assert_eq!(ctx.csr.mcycle, sail_ctx.into_virt_context().csr.mcycle, "read csr equivalence");
+        assert_eq!(ctx.csr.minstret, sail_ctx.into_virt_context().csr.minstret, "read csr equivalence");
+        assert_eq!(ctx.csr.mcountinhibit, sail_ctx.into_virt_context().csr.mcountinhibit, "read csr equivalence");
+        assert_eq!(ctx.csr.mcounteren, sail_ctx.into_virt_context().csr.mcounteren, "read csr equivalence");
+        assert_eq!(ctx.csr.menvcfg, sail_ctx.into_virt_context().csr.menvcfg, "read csr equivalence");
+        assert_eq!(ctx.csr.mseccfg, sail_ctx.into_virt_context().csr.mseccfg, "read csr equivalence");
+        assert_eq!(ctx.csr.mcause, sail_ctx.into_virt_context().csr.mcause, "read csr equivalence");
+        assert_eq!(ctx.csr.mepc, sail_ctx.into_virt_context().csr.mepc, "read csr equivalence");
+        assert_eq!(ctx.csr.mtval, sail_ctx.into_virt_context().csr.mtval, "read csr equivalence");
+        assert_eq!(ctx.csr.mtval2, sail_ctx.into_virt_context().csr.mtval2, "read csr equivalence");
+        assert_eq!(ctx.csr.mstatus, sail_ctx.into_virt_context().csr.mstatus, "read csr equivalence");
+        assert_eq!(ctx.csr.mtinst, sail_ctx.into_virt_context().csr.mtinst, "read csr equivalence");
+        assert_eq!(ctx.csr.mconfigptr, sail_ctx.into_virt_context().csr.mconfigptr, "read csr equivalence");
+        assert_eq!(ctx.csr.stvec, sail_ctx.into_virt_context().csr.stvec, "read csr equivalence");
+        assert_eq!(ctx.csr.scounteren, sail_ctx.into_virt_context().csr.scounteren, "read csr equivalence");
+        assert_eq!(ctx.csr.senvcfg, sail_ctx.into_virt_context().csr.senvcfg, "read csr equivalence");
+        assert_eq!(ctx.csr.sscratch, sail_ctx.into_virt_context().csr.sscratch, "read csr equivalence");
+        assert_eq!(ctx.csr.sepc, sail_ctx.into_virt_context().csr.sepc, "read csr equivalence");
+        assert_eq!(ctx.csr.scause, sail_ctx.into_virt_context().csr.scause, "read csr equivalence");
+        assert_eq!(ctx.csr.stval, sail_ctx.into_virt_context().csr.stval, "read csr equivalence");
+        assert_eq!(ctx.csr.satp, sail_ctx.into_virt_context().csr.satp, "read csr equivalence");
+        assert_eq!(ctx.csr.scontext, sail_ctx.into_virt_context().csr.scontext, "read csr equivalence");
+        assert_eq!(ctx.csr.medeleg, sail_ctx.into_virt_context().csr.medeleg, "read csr equivalence");
+        assert_eq!(ctx.csr.mideleg, sail_ctx.into_virt_context().csr.mideleg, "read csr equivalence");
+        assert_eq!(ctx.csr.pmpcfg, sail_ctx.into_virt_context().csr.pmpcfg, "read csr equivalence");
+        assert_eq!(ctx.csr.pmpaddr, sail_ctx.into_virt_context().csr.pmpaddr, "read csr equivalence");
+        assert_eq!(ctx.csr.mhpmcounter, sail_ctx.into_virt_context().csr.mhpmcounter, "read csr equivalence");
+        assert_eq!(ctx.csr.mhpmevent, sail_ctx.into_virt_context().csr.mhpmevent, "read csr equivalence");
     }
 }
