1b487e7fd7b41b8964ce396684ee8e8e5e5b91a4
François Costa <frankost.costa@gmail.com>
2025-01-03 18:32:37 +0100

    Add formal verification for fence operations
    This commit introduces the formal verification for fence operations. Note that Miralis forwards the call to the hardware. Therefore, we assert that there is no modification in the virtual context after this fence.

--- model_checking/src/execute.rs
+++ model_checking/src/execute.rs
@@ -10,8 +10,14 @@ pub fn execute_ast(sail_virt_ctx: &mut SailVirtCtx, instr: usize) {
         ast::WFI(()) => {
             sail_model::execute_WFI(sail_virt_ctx);
         }
-        ast::SFENCE_VMA((_rs1, _rs2)) => {
-            // TODO: Implement this part
+        ast::SFENCE_VMA((rs1, rs2)) => {
+            sail_model::execute_SFENCE_VMA(sail_virt_ctx, rs1, rs2);
+        }
+        ast::HFENCE_VVMA((rs1, rs2)) => {
+            sail_model::execute_HFENCE_VVMA(sail_virt_ctx, rs1, rs2);
+        }
+        ast::HFENCE_GVMA((rs1, rs2)) => {
+            sail_model::execute_HFENCE_GVMA(sail_virt_ctx, rs1, rs2);
         }
         ast::CSR((csr, rs1, rd, is_imm, op)) => {
             sail_model::execute_CSR(sail_virt_ctx, csr, rs1, rd, is_imm, op);
