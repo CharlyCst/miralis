3275fa068b0495f8f0ddb82351a51baa9707204a
François Costa <frankost.costa@gmail.com>
2025-02-22 13:42:37 +0100

    Add support for measuring exceptions by categories in  `counter benchmark`
    Before this commit, the `counter benchmark` measured the number of world
    switches and firmware trap. This commit refines a bit the granularity by
    adding a few other categories such as request for time, set time, IPI
    broadcast, remote fence and misaligned instruction.
    
    NOTE: because of the slight refactor of the benchmark interface this
    commit deprecates the old `default` benchmark. As the benchmark is no
    longer accurate we remove the corresponding tests.

--- src/benchmark/mod.rs
+++ src/benchmark/mod.rs
@@ -8,8 +8,13 @@ mod default;
 mod empty;
 
 use config_select::select_env;
+use miralis_core::sbi_codes::{
+    is_i_fence_request, is_ipi_request, is_timer_request, is_vma_request,
+};
 
-use crate::virt::VirtContext;
+use crate::arch::{MCause, Register};
+use crate::virt::traits::RegisterContextGetter;
+use crate::virt::{ExecutionMode, VirtContext};
 
 pub type Benchmark = select_env!["MIRALIS_BENCHMARK_TYPE":
     "default"      => default::DefaultBenchmark
@@ -24,7 +29,12 @@ pub trait BenchmarkModule {
 
     fn start_interval_counters(_scope: Scope) {}
     fn stop_interval_counters(_scope: Scope) {}
-    fn increment_counter(_ctx: &mut VirtContext, _counter: Counter) {}
+    fn increment_counter(
+        _ctx: &mut VirtContext,
+        _from_exec_mode: ExecutionMode,
+        _to_exec_mode: ExecutionMode,
+    ) {
+    }
 
     /// Print formated string with value of the counters
     fn display_counters() {}
@@ -63,4 +73,55 @@ pub enum Counter {
     TotalExits = 0,
     FirmwareExits = 1,
     WorldSwitches = 2,
+}
+
+pub enum ExceptionCategory {
+    NotOffloaded = 0,
+    ReadTime = 1,
+    SetTimer = 2,
+    MisalignedOp = 3,
+    IPI = 4,
+    RemoteFence = 5,
+    FirmwareTrap = 6,
+}
+
+pub fn get_exception_category(
+    ctx: &mut VirtContext,
+    from_exec_mode: ExecutionMode,
+    to_exec_mode: ExecutionMode,
+) -> Option<ExceptionCategory> {
+    match (from_exec_mode, to_exec_mode) {
+        (ExecutionMode::Payload, ExecutionMode::Firmware) => Some(ExceptionCategory::NotOffloaded),
+        (ExecutionMode::Firmware, ExecutionMode::Firmware) => Some(ExceptionCategory::FirmwareTrap),
+        (ExecutionMode::Payload, ExecutionMode::Payload) => {
+            match MCause::try_from(ctx.trap_info.mcause).unwrap() {
+                MCause::StoreAddrMisaligned | MCause::LoadAddrMisaligned => {
+                    Some(ExceptionCategory::MisalignedOp)
+                }
+                MCause::IllegalInstr => Some(ExceptionCategory::ReadTime),
+                MCause::EcallFromSMode
+                    if is_timer_request(ctx.get(Register::X16), ctx.get(Register::X17)) =>
+                {
+                    Some(ExceptionCategory::SetTimer)
+                }
+                MCause::EcallFromSMode
+                    if is_ipi_request(ctx.get(Register::X16), ctx.get(Register::X17)) =>
+                {
+                    Some(ExceptionCategory::IPI)
+                }
+                MCause::EcallFromSMode
+                    if is_i_fence_request(ctx.get(Register::X16), ctx.get(Register::X17)) =>
+                {
+                    Some(ExceptionCategory::RemoteFence)
+                }
+                MCause::EcallFromSMode
+                    if is_vma_request(ctx.get(Register::X16), ctx.get(Register::X17)) =>
+                {
+                    Some(ExceptionCategory::RemoteFence)
+                }
+                _ => None,
+            }
+        }
+        _ => None,
+    }
 }
