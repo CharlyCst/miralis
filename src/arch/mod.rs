77b3b5b47abbfdeed2aab051a663f01a8b2578fe
Charly Castes <charly.castes@epfl.ch>
2025-07-16 11:02:25 +0200

    Disable LCOFIE until upstream Sail support
    The LCOFIE interrupt (for counter overflow) has been removed from Sail,
    but will be added back later. Until it is reintroduced, we remove
    support in Miralis.

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -385,7 +385,8 @@ pub mod mstatus {
 #[allow(unused)]
 pub mod mie {
     /// Constant to filter out SIE bits of mstatus
-    pub const SIE_FILTER: usize = SSIE_FILTER | STIE_FILTER | SEIE_FILTER | LCOFIE_FILTER;
+    //  Note: LCOFIE is not yet supported in the upstream Sail model
+    pub const SIE_FILTER: usize = SSIE_FILTER | STIE_FILTER | SEIE_FILTER /* | LCOFIE_FILTER */;
 
     /// Constant to filter out writable bits of mie.
     pub const MIE_WRITE_FILTER: usize = SIE_FILTER | MSIE_FILTER | MTIE_FILTER | MEIE_FILTER;
@@ -436,8 +437,8 @@ pub mod mie {
         | STIE_FILTER
         | MTIE_FILTER
         | SEIE_FILTER
-        | MEIE_FILTER
-        | LCOFIE_FILTER;
+        | MEIE_FILTER;
+        // | LCOFIE_FILTER; // Not yet supported in the Sail model
 }
 
 // ———————————————————— Machine Trap-Vector Base-Address ———————————————————— //
